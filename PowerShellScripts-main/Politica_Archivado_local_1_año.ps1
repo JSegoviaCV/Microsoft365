8fef98c0fc8b4e42227be071bfd23b3992e0cb1e
Jonathan Eduardo Segovia <132959708+JSegoviaCV@users.noreply.github.com>
2023-05-10 11:52:06 -0300

    Create massive users

--- PowerShellScripts-main/Politica_Archivado_local_1_año.ps1
+++
@@ -1,5 +0,0 @@
-﻿Connect-ExchangeOnline
-
-Get-Mailbox -ResultSize unlimited| Select Displayname, RetentionPolicy
-
-Get-Mailbox -ResultSize unlimited | Set-Mailbox -RetentionPolicy "Retener 1 año a usuarios gerentes"
